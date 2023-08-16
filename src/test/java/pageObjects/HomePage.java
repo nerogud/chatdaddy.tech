362e28d725688b5288cdc2ea29acefbf3c7202d1
nerogud <nero.ogude@yahoo.com>
2023-08-16 21:09:08 +0100

    HomePage

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class HomePage extends BasePage{
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	@FindBy(css="img[alt='logo']")
+	WebElement chatdaddyLogo;
+	
+	@FindBy(css="div[role='button']")
+	WebElement languageOptionDropdownButton;
+	
+	public boolean chatdaddyLogoIsDisplayed() {
+		return chatdaddyLogo.isDisplayed();
+	}
+	public boolean languageOptionDropdownButtonIsDisplayed() {
+		return languageOptionDropdownButton.isDisplayed();
+	}
+	public boolean languageOptionDropdownButtonIsEnabled() {
+		return languageOptionDropdownButton.isEnabled();
+	}
+	public void selectEnglishLanguageOption() {
+		Select Language = new Select(languageOptionDropdownButton);
+		Language.selectByVisibleText("English");
+	}
+	public void selectEspanolLanguageOption() {
+		Select Language = new Select(languageOptionDropdownButton);
+		Language.selectByIndex(5);;
+	}
+	
+	
+	
+
+}
