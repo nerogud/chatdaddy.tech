d2b6f3f223c079f6410b77df7613f9c03a0f83a5
nerogud <nero.ogude@yahoo.com>
2023-08-17 18:42:05 +0100

    update BaseClass

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,7 +1,9 @@
 package testBase;
 
 import java.time.Duration;
+import java.util.ResourceBundle;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,9 +14,13 @@ public class BaseClass {
 	
 	public static WebDriver driver;
 	public ChromeOptions ops;
+	public ResourceBundle resourcebundle;
 	
 	@BeforeClass
 	public void setup() {
+		resourcebundle= ResourceBundle.getBundle("config");
+		
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\okeog\\OneDrive\\Documents\\Nero Eclipse\\chatdaddy.tech\\drivers\\chromedriver.exe");
 		ops = new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
@@ -23,12 +29,19 @@ public class BaseClass {
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-		driver.get("https://app.chatdaddy.tech/");
+		driver.get(resourcebundle.getString("url"));
 	}
 	
 	@AfterClass
 	public void teardown() {
 		driver.quit();
+	}
+	
+	public String getRandomString() {
+		return RandomStringUtils.randomAlphabetic(7);
+	}
+	public String getRandomNumbers() {
+		return RandomStringUtils.randomNumeric(7);
 	}
 	
 
