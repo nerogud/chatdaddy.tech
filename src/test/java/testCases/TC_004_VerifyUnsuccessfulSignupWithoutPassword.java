a23b8bc942f3aedec161671a351882115ab2bf4d
nerogud <nero.ogude@yahoo.com>
2023-08-23 18:13:10 +0100

    TC_004

---
+++ src/test/java/testCases/TC_004_VerifyUnsuccessfulSignupWithoutPassword.java
@@ -0,0 +1,38 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.SignupPage;
+import pageObjects.SignupVerificationPage;
+import testBase.BaseClass;
+
+public class TC_004_VerifyUnsuccessfulSignupWithoutPassword extends BaseClass{
+	
+	@Test
+	public void UnsuccessfulSignupWithoutFullName() {
+		
+		logger.info("***Start VerifyUnsuccessfulSignupWithoutFullName Test***");
+		HomePage homepage = new HomePage(driver);
+		homepage.clickSignupButton();
+		logger.info("***Signup button clicked***");
+		
+		SignupPage signuppage = new SignupPage(driver);
+		
+		logger.info("***Enter user details***");
+		signuppage.selectCountryCallCode();
+		signuppage.setFullNameInputField(resourcebundle.getString("FullName"));
+		signuppage.setPhoneNumberInputField(resourcebundle.getString("PhoneNumber"));
+		//intentionally keep Password Field empty
+		
+		
+		logger.info("***Assert Test***");
+		Assert.assertFalse(signuppage.registerButtonIsEnabled());
+		
+		
+	}
+	
+	
+
+}
